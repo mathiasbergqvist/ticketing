fd82e1de40f857c3170e9d0696fc32ba86531254
Mathias <mathiasbergqvist88@gmail.com>
2021-02-01 07:24:46 +0100

    Scaffold routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,10 @@
 import express from "express";
 import { json } from "body-parser";
+import { currentUserRouter } from "./routes/current-user";
+import { signInRouter } from "./routes/singin";
+import { signOutRouter } from "./routes/signout";
+import { signUpRouter } from "./routes/signup";
+
 const chalk = require("chalk");
 const log = console.log;
 const info = chalk.blue;
@@ -8,9 +13,10 @@ const error = chalk.red;
 const app = express();
 app.use(json());
 
-app.get("api/users/currentuser", (req, res) => {
-  res.send("Hi there!");
-});
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
 
 app.listen(3001, () => {
   log(info("AUTH: Server started on port 3001 🚀"));
