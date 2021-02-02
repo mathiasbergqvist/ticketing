d871774825368cc4399dae1876c2c6a0d65253c0
Mathias <mathiasbergqvist88@gmail.com>
2021-02-02 09:11:21 +0100

    Determinig error type

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
+import { DataBaseConnectionError } from "./../errors/database-connection-error";
 import { Request, Response, NextFunction } from "express";
+import { RequestValidationError } from "./../errors/request-validation-errors";
 
 export const errorHandler = (
   err: Error,
@@ -6,7 +8,13 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log("Something went wrong", err);
+  if (err instanceof RequestValidationError) {
+    console.log("RequestValidationError", err);
+  }
+
+  if (err instanceof DataBaseConnectionError) {
+    console.log("DataBaseConnectionError", err);
+  }
 
   res.status(405).send({
     message: err.message,
