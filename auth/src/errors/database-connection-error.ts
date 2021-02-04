6a20f669693edeb71d1d9465750d1e96a1efb6d8
Mathias <mathiasbergqvist88@gmail.com>
2021-02-04 09:27:37 +0100

    Moving logic into errors

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,9 +1,14 @@
 export class DataBaseConnectionError extends Error {
+  statusCode = 500;
   reason = "Error connecting to database";
 
   constructor() {
     super();
     // Only because we are extending a built in class
     Object.setPrototypeOf(this, DataBaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.reason }];
   }
 }
