770d5458fd7f0e12643b37800999b29d9a2c0dbd
Mathias <mathiasbergqvist88@gmail.com>
2021-02-05 08:44:44 +0100

    New custom error for routes not found

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,7 @@ import { signInRouter } from "./routes/singin";
 import { signOutRouter } from "./routes/signout";
 import { signUpRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 import { log, info } from "./logger";
 
 const app = express();
@@ -14,6 +15,11 @@ app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
 app.use(errorHandler);
 
 app.listen(3001, () => {
