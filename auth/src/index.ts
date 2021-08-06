6841c922c11e413e96b033339009d747ec96ef6f
Mathias <mathiasbergqvist88@gmail.com>
2021-08-06 14:23:13 +0200

    Index to app refactor

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,35 +1,6 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
-import { signInRouter } from "./routes/singin";
-import { signOutRouter } from "./routes/signout";
-import { signUpRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
 import { log, info, error } from "./logger";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-import { currentUserRouter } from "./routes/current-user";
-
-const app = express();
-app.set("trust proxy", true); // Allow traffic through ngnx
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-app.use(signUpRouter);
-
-app.all("*", () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from "./app";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
