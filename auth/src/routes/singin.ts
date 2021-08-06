c3dd7ff43ad4cd7585b7716886e4010f7ac408ec
Mathias <mathiasbergqvist88@gmail.com>
2021-08-06 10:45:05 +0200

    The signin flow

--- auth/src/routes/singin.ts
+++ auth/src/routes/singin.ts
@@ -1,9 +1,26 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-errors";
+import { error } from "../logger";
 
 const router = express.Router();
 
-router.get("/api/users/signin", (req, res) => {
-  res.send("Foo bar");
-});
+router.get(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("You must supply a password"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+  }
+);
 
 export { router as signInRouter };
