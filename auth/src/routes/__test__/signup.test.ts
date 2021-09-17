9bed53dc16267b7271051921493ac23201b494ad
Mathias <mathiasbergqvist88@gmail.com>
2021-09-17 10:00:02 +0200

    Requiring unique emails

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -1,36 +1,56 @@
 import request from "supertest";
 import { app } from "../../app";
 
-it("returns 201 on successful signup", async () => {
-  return request(app)
-    .post("/api/users/signup")
-    .send({
-      email: "test@test.com",
-      password: "pwd123",
-    })
-    .expect(201);
-});
+describe("signup", () => {
+  it("returns 201 on successful signup", async () => {
+    return request(app)
+      .post("/api/users/signup")
+      .send({
+        email: "test@test.com",
+        password: "pwd123",
+      })
+      .expect(201);
+  });
 
-it("returns 400 on invalid email", async () => {
-  return request(app)
-    .post("/api/users/signup")
-    .send({
-      email: "test.com",
-      password: "pwd123",
-    })
-    .expect(400);
-});
+  it("returns 400 on invalid email", async () => {
+    return request(app)
+      .post("/api/users/signup")
+      .send({
+        email: "test.com",
+        password: "pwd123",
+      })
+      .expect(400);
+  });
 
-it("returns 400 on invalid password", async () => {
-  return request(app)
-    .post("/api/users/signup")
-    .send({
-      email: "test@test.com",
-      password: "a",
-    })
-    .expect(400);
-});
+  it("returns 400 on invalid password", async () => {
+    return request(app)
+      .post("/api/users/signup")
+      .send({
+        email: "test@test.com",
+        password: "a",
+      })
+      .expect(400);
+  });
 
-it("returns 400 with missing email and password", async () => {
-  return request(app).post("/api/users/signup").send({}).expect(400);
+  it("returns 400 with missing email and password", async () => {
+    return request(app).post("/api/users/signup").send({}).expect(400);
+  });
+
+  it("disallowes duplicate emails", async () => {
+    await request(app)
+      .post("/api/users/signup")
+      .send({
+        email: "test@test.com",
+        password: "password",
+      })
+      .expect(201);
+
+    await request(app)
+      .post("/api/users/signup")
+      .send({
+        email: "test@test.com",
+        password: "password",
+      })
+      .expect(400);
+  });
 });
