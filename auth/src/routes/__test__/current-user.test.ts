265ca2cd2276b52884e2ea01a434fa70a02af63a
Mathias <mathiasbergqvist88@gmail.com>
2021-09-17 15:02:51 +0200

    Testing non-authed requests

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -13,4 +13,13 @@ describe("current user", () => {
 
     expect(response.body.currentUser.email).toEqual("test@test.com");
   });
+
+  it("responds with null if not authenticated", async () => {
+    const response = await request(app)
+      .get("/api/users/currentuser")
+      .send()
+      .expect(200);
+
+    expect(response.body.currentUser).toBeNull();
+  });
 });
