d66bae07c9d7951392ac7dc238c4bd5e5c7930b1
Mathias <mathiasbergqvist88@gmail.com>
2021-09-20 08:51:11 +0200

    Successful account signup

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    const response = await axios.post("/api/users/signup", {
+      email,
+      password,
+    });
   };
 
   return (
