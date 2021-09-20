2c73e852cbacca8a7acd1e1118206723ea5150f2
Mathias <mathiasbergqvist88@gmail.com>
2021-09-20 09:00:43 +0200

    Handling validation errors

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -4,14 +4,19 @@ import axios from "axios";
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await axios.post("/api/users/signup", {
-      email,
-      password,
-    });
+    try {
+      const response = await axios.post("/api/users/signup", {
+        email,
+        password,
+      });
+    } catch (error) {
+      setErrors(error.response.data.errors);
+    }
   };
 
   return (
@@ -34,6 +39,16 @@ const Signup = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
       </div>
+      {errors.length > 0 && (
+        <div className="alert alert-danger">
+          <h4>Ooops...</h4>
+          <ul className="my-0">
+            {errors.map((error) => (
+              <li key={error.message}>{error.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
       <button className="btn btn-primary" type="submit">
         Sign Up
       </button>
