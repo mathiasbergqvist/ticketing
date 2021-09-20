e3568ff3d43c30c60c83bbb17378075e180ce601
Mathias <mathiasbergqvist88@gmail.com>
2021-09-20 09:28:48 +0200

    Using the useRequest hook.

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,11 +1,19 @@
 import axios from "axios";
 import { useState } from "react";
 
-export default ({ url, method = "post", body }) => {
+export const METHOD = {
+  POST: "post",
+  GET: "get",
+  PUT: "put",
+  DELETE: "delete",
+};
+
+export default ({ url, method = METHOD.POST, body }) => {
   const [errors, setErrors] = useState(null);
 
   const doRequest = async () => {
     try {
+      setErrors(null);
       const response = await axios[method](url, body);
       return response.data;
     } catch (error) {
@@ -13,7 +21,7 @@ export default ({ url, method = "post", body }) => {
         <div className="alert alert-danger">
           <h4>Ooops...</h4>
           <ul className="my-0">
-            {error.response.data.errors.map((error) => (
+            {error.response?.data?.errors?.map((error) => (
               <li key={error.message}>{error.message}</li>
             ))}
           </ul>
--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
 import axios from "axios";
+import useRequest, { METHOD } from "../../hooks/use-request";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState([]);
+  const { doRequest, errors } = useRequest({
+    url: "/api/users/signup",
+    method: METHOD.POST,
+    body: {
+      email,
+      password,
+    },
+  });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    try {
-      const response = await axios.post("/api/users/signup", {
-        email,
-        password,
-      });
-    } catch (error) {
-      setErrors(error.response.data.errors);
-    }
+    doRequest();
   };
 
   return (
@@ -39,16 +39,7 @@ const Signup = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
       </div>
-      {errors.length > 0 && (
-        <div className="alert alert-danger">
-          <h4>Ooops...</h4>
-          <ul className="my-0">
-            {errors.map((error) => (
-              <li key={error.message}>{error.message}</li>
-            ))}
-          </ul>
-        </div>
-      )}
+      {errors}
       <button className="btn btn-primary" type="submit">
         Sign Up
       </button>
