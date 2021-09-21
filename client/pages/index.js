a2074209cdfca6218bb59f32045f011da8599093
Mathias <mathiasbergqvist88@gmail.com>
2021-09-21 09:11:02 +0200

    Fetching data during ssr

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,17 @@
 import React from "react";
+import axios from "axios";
 
-const Landing = () => {
+const Landing = ({ currentData }) => {
   return <h1>Landing page</h1>;
+};
+
+Landing.getInitialProps = async () => {
+  try {
+    const response = await axios.get("/api/users/currentuser");
+    return response.data;
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 export default Landing;
