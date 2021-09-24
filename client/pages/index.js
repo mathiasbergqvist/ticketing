cb89ae324134534fde9d8daecf9fa6f5f5cf55c3
Mathias <mathiasbergqvist88@gmail.com>
2021-09-24 09:15:55 +0200

    Specifying the host

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,16 +1,33 @@
 import React from "react";
 import axios from "axios";
 
-const Landing = ({ currentData }) => {
+const Landing = ({ currentUser }) => {
   return <h1>Landing page</h1>;
 };
 
 Landing.getInitialProps = async () => {
-  try {
-    const response = await axios.get("/api/users/currentuser");
-    return response.data;
-  } catch (err) {
-    console.error(err);
+  // Check if server side
+  if (typeof window === "undefined") {
+    try {
+      const { data } = await axios.get(
+        "http://ingress-nginx-controller.ingress-nginx.svc.clusster.local/api/users/currentuser",
+        {
+          headers: {
+            Host: "ticketing.dev",
+          },
+        }
+      );
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
+  } else {
+    try {
+      const { data } = await axios.get("/api/users/currentuser");
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
   }
 };
 
